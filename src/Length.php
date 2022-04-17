0c29738af4f97bc349d9482664cec559828dd3a5
Marcelo Rocha <contato@omarcelo.com.br>
2022-04-17 09:52:15 -0300

    Added required constants to Length class

---
+++ src/Length.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace RochaMarcelo\MetricsConversion;
+
+class Length
+{
+    public const MILE = 'mi';
+    public const MILLIMETER = 'mm';
+    public const YARD = 'yd';
+    public const METER = 'm';
+    public const KILOMETER = 'km';
+    public const INCH = 'in';
+    public const CENTIMETER = 'cm';
+    public const FEET = 'ft';
+}
