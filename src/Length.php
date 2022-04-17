fca4cc61abc240b6192926b926c37fed4192cdbc
Marcelo Rocha <contato@omarcelo.com.br>
2022-04-17 19:30:15 -0300

    Added helpers methods to handle length conversion using value objects, for each unit one method (toInch, toMeter, etc)

--- src/Length.php
+++ src/Length.php
@@ -6,12 +6,113 @@ namespace RochaMarcelo\MetricsConversion;
 
 class Length
 {
+    public const CENTIMETER = 'cm';
+    public const FEET = 'ft';
+    public const INCH = 'in';
+    public const KILOMETER = 'km';
+    public const METER = 'm';
     public const MILE = 'mi';
     public const MILLIMETER = 'mm';
     public const YARD = 'yd';
-    public const METER = 'm';
-    public const KILOMETER = 'km';
-    public const INCH = 'in';
-    public const CENTIMETER = 'cm';
-    public const FEET = 'ft';
+
+    /**
+     * @var string[]
+     */
+    public const LIST = [
+        self::CENTIMETER,
+        self::FEET,
+        self::INCH,
+        self::KILOMETER,
+        self::METER,
+        self::MILE,
+        self::MILLIMETER,
+        self::YARD,
+    ];
+
+    /**
+     * @var float|int
+     */
+    private $value;
+
+    /**
+     * @var string
+     */
+    private string $unit;
+
+    /**
+     * @param float|int $value The length value.
+     * @param string $unit The base unit of the value.
+     */
+    public function __construct($value, string $unit)
+    {
+        if (!in_array($unit, self::LIST)) {
+            throw new \InvalidArgumentException(sprintf('Invalid length unit "%s".', $unit));
+        }
+        $this->value = $value;
+        $this->unit = $unit;
+    }
+
+    /**
+     * @return float
+     */
+    public function toCentimeters(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::CENTIMETER);
+    }
+
+    /**
+     * @return float
+     */
+    public function toFeet(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::FEET);
+    }
+
+    /**
+     * @return float
+     */
+    public function toInches(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::INCH);
+    }
+
+    /**
+     * @return float
+     */
+    public function toKilometers(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::KILOMETER);
+    }
+
+    /**
+     * @return float
+     */
+    public function toMeters(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::METER);
+    }
+
+    /**
+     * @return float
+     */
+    public function toMiles(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::MILE);
+    }
+
+    /**
+     * @return float
+     */
+    public function toMillimeters(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::MILLIMETER);
+    }
+
+    /**
+     * @return float
+     */
+    public function toYards(): float
+    {
+        return LengthTable::convert($this->value, $this->unit, self::YARD);
+    }
 }
---
+++ tests/TestCase/LengthTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RochaMarcelo\MetricsConversion\Test\TestCase;
+
+use PHPUnit\Framework\TestCase;
+use RochaMarcelo\MetricsConversion\Length;
+
+/**
+ * @coversDefaultClass \RochaMarcelo\MetricsConversion\Length
+ */
+class LengthTest extends TestCase
+{
+    /**
+     * @covers ::toInches
+     * @covers ::toFeet
+     * @covers ::toKilometers
+     * @covers ::toMeters
+     * @covers ::toMiles
+     * @covers ::toMillimeters
+     * @covers ::toYards
+     * @covers ::toCentimeters
+     *
+     * @return void
+     */
+    public function testToUnitAll()
+    {
+        $length = new Length(15345.345323, Length::CENTIMETER);
+        $this->assertEquals(503.4562113845144, $length->toFeet());
+        $feet = new Length(503.4562113845144, Length::FEET);
+        $this->assertEquals(6041.474536614173, $length->toInches());
+        $this->assertEquals(6041.474536614173, $feet->toInches());
+        $inches = new Length(6041.474536614173, Length::INCH);
+        $this->assertEquals(0.15345345323, $length->toKilometers());
+        $this->assertEquals(0.153454, round($inches->toKilometers(), 6));
+        $km = new Length(0.15345345323, Length::KILOMETER);
+        $this->assertEquals(153.45345323, $length->toMeters());
+        $this->assertEquals(153.45345323, $km->toMeters());
+        $meters = new Length(153.45345323, Length::METER);
+        $this->assertEquals(0.09535155518646107, $length->toMiles());
+        $this->assertEquals(0.09535155518646107, $meters->toMiles());
+        $miles = new Length(0.09535155518646107, Length::MILE);
+        $this->assertEquals(153453.45323, $length->toMillimeters());
+        $this->assertEquals(153453.45323, $miles->toMillimeters());
+        $miles = new Length(153453.45323, Length::MILE);
+        $this->assertEquals(167.81873712817148, $length->toYards());
+        $yards = new Length(167.81873712817148, Length::YARD);
+        $this->assertEquals(15345.345323, $length->toCentimeters());
+        $this->assertEquals(15345.345323, $yards->toCentimeters());
+    }
+
+    /**
+     * @covers ::__construct
+     * @return void
+     */
+    public function testCreationWithInvalidUnit()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid length unit "gm".');
+        new Length(10, 'gm');
+    }
+}
