42010a10791111b86f2f86d2fcde57e76b4dc874
Marcelo Rocha <contato@omarcelo.com.br>
2022-04-17 11:31:19 -0300

    Added length conversion logic

---
+++ src/LengthTable.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RochaMarcelo\MetricsConversion;
+
+class LengthTable
+{
+    /**
+     * @var float[][]
+     */
+    protected static array $multipliers = [
+        Length::INCH => [
+            Length::INCH => 1.0,
+            Length::MILLIMETER => 25.4,
+            Length::CENTIMETER => 2.54,
+        ],
+        Length::FEET => [
+            Length::FEET => 1.0,
+            Length::INCH => 12.0,
+            Length::MILLIMETER => 304.8,
+            Length::CENTIMETER => 30.48,
+            Length::METER => 0.3048,
+        ],
+        Length::YARD => [
+            Length::YARD => 1.0,
+            Length::INCH => 36.0,
+            Length::FEET => 3.0,
+            Length::MILLIMETER => 914.4,
+            Length::CENTIMETER => 91.44,
+            Length::METER => 0.9144,
+            Length::KILOMETER => 0.0009144,
+        ],
+        Length::MILE => [
+            Length::INCH => 63360,
+            Length::FEET => 5280,
+            Length::YARD => 1760,
+            Length::MILE => 1.0,
+            Length::MILLIMETER => 1609344,
+            Length::CENTIMETER => 160934.4,
+            Length::METER => 1609.344,
+            Length::KILOMETER => 1.609344,
+        ],
+        Length::MILLIMETER => [
+            Length::MILLIMETER => 1.0,
+        ],
+        Length::CENTIMETER => [
+            Length::MILLIMETER => 10.0,
+            Length::CENTIMETER => 1.0,
+        ],
+        Length::METER => [
+            Length::FEET => 3.280839895,
+            Length::MILLIMETER => 1000.0,
+            Length::CENTIMETER => 100.0,
+            Length::METER => 1.0,
+        ],
+        Length::KILOMETER => [
+            Length::INCH => 39370.1,
+            Length::FEET => 3280.84,
+            Length::MILLIMETER => 1000000,
+            Length::CENTIMETER => 100000,
+            Length::METER => 1000,
+            Length::KILOMETER => 1.0,
+        ],
+    ];
+
+    /**
+     * @var float[][]
+     */
+    protected static array $divisors = [
+        Length::INCH => [
+            Length::FEET => 12.0,
+            Length::YARD => 36.0,
+            Length::MILE => 63360.0,
+            Length::METER => 39.37,
+            Length::KILOMETER => 39370.0,
+        ],
+        Length::FEET => [
+            Length::YARD => 3.0,
+            Length::MILE => 5280.0,
+            Length::KILOMETER => 3280.84,
+        ],
+        Length::YARD => [
+            Length::MILE => 1760.0,
+        ],
+        Length::MILLIMETER => [
+            Length::INCH => 25.4,
+            Length::FEET => 304.8,
+            Length::YARD => 914.4,
+            Length::MILE => 1609344.0,
+            Length::CENTIMETER => 10.0,
+            Length::METER => 1000.0,
+            Length::KILOMETER => 1e+6,
+        ],
+        Length::CENTIMETER => [
+            Length::INCH => 2.54,
+            Length::FEET => 30.48,
+            Length::YARD => 91.44,
+            Length::MILE => 160934.4,
+            Length::METER => 100.0,
+            Length::KILOMETER => 100000.0,
+        ],
+        Length::METER => [
+            Length::INCH => 0.0254,
+            Length::YARD => 0.9144,
+            Length::MILE => 1609.344,
+            Length::KILOMETER => 1000.0,
+        ],
+        Length::KILOMETER => [
+            Length::YARD => 0.0009144,
+            Length::MILE => 1.609344,
+        ]
+    ];
+
+    /**
+     * @param float|int $value The value to convert.
+     * @param string $from The value current measure type.
+     * @param string $to The measure type to convert.
+     * @return float
+     */
+    public static function convert($value, string $from, string $to): float
+    {
+        if (isset(static::$multipliers[$from][$to])) {
+            return $value * static::$multipliers[$from][$to];
+        }
+        if (isset(static::$divisors[$from][$to])) {
+            return $value / static::$divisors[$from][$to];
+        }
+        throw new \InvalidArgumentException(
+            sprintf('Can\'t convert value from "%s" to "%s"', $from, $to)
+        );
+    }
+}
---
+++ tests/TestCase/LengthTableTest.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RochaMarcelo\MetricsConversion\Test\TestCase;
+
+use RochaMarcelo\MetricsConversion\Length;
+use RochaMarcelo\MetricsConversion\LengthTable;
+
+/**
+ *
+ */
+class LengthTableTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test convert method.
+     *
+     * @param float|int $value The value to test as input for convert metho.
+     * @param string $measureFrom The base measure to convert from.
+     * @param string $measureTo The measure to convert to.
+     * @param float $expectedResult The expected result.
+     *
+     * @dataProvider dataProviderConvert
+     * @return void
+     */
+    public function testConvert($value, string $measureFrom, string $measureTo, float $expectedResult)
+    {
+        $actual = LengthTable::convert($value, $measureFrom, $measureTo);
+
+        $this->assertSame($expectedResult, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderConvert(): array
+    {
+        return [
+            //From inch
+            [1, Length::INCH, Length::INCH, 1,],
+            [1, Length::INCH, Length::FEET, 0.08333333333333333,],
+            [1, Length::INCH, Length::YARD, 1 / 36,],
+            [1, Length::INCH, Length::MILE, 1.5782828282828E-5,],
+            [1, Length::INCH, Length::MILLIMETER, 25.4,],
+            [1, Length::INCH, Length::CENTIMETER, 2.54,],
+            [1, Length::INCH, Length::METER, 0.025400050800101603,],
+            [1, Length::INCH, Length::KILOMETER, 1 / 39370,],
+            [10, Length::INCH, Length::INCH, 10,],
+            [10, Length::INCH, Length::FEET, 0.8333333333333334,],
+            [10, Length::INCH, Length::YARD, 0.2777777777777778,],
+            [10, Length::INCH, Length::MILE, 0.00015782828282828284,],
+            [10, Length::INCH, Length::MILLIMETER, 254,],
+            [10, Length::INCH, Length::CENTIMETER, 25.4,],
+            [10, Length::INCH, Length::METER, 0.25400050800101603,],
+            [10, Length::INCH, Length::KILOMETER, 0.000254000508001016,],
+            //from feet
+            [1, Length::FEET, Length::INCH, 12,],
+            [1, Length::FEET, Length::FEET, 1,],
+            [1, Length::FEET, Length::YARD, (1 / 3),],
+            [1, Length::FEET, Length::MILE, (1 / 5280),],
+            [1, Length::FEET, Length::MILLIMETER, 304.8,],
+            [1, Length::FEET, Length::CENTIMETER, 30.48,],
+            [1, Length::FEET, Length::METER, 0.3048,],
+            [1, Length::FEET, Length::KILOMETER, 1 / 3280.84,],
+            [10, Length::FEET, Length::INCH, 120,],
+            [10, Length::FEET, Length::FEET, 10,],
+            [10, Length::FEET, Length::YARD, (10 / 3),],
+            [10, Length::FEET, Length::MILE, (10 / 5280),],
+            [10, Length::FEET, Length::MILLIMETER, 3048,],
+            [10, Length::FEET, Length::CENTIMETER, 304.8,],
+            [10, Length::FEET, Length::METER, 3.048,],
+            [10, Length::FEET, Length::KILOMETER, 10 / 3280.84,],
+            //from yard
+            [1, Length::YARD, Length::INCH, 36,],
+            [1, Length::YARD, Length::FEET, 3,],
+            [1, Length::YARD, Length::YARD, 1,],
+            [1, Length::YARD, Length::MILE, (1 / 1760),],
+            [1, Length::YARD, Length::MILLIMETER, 914.4,],
+            [1, Length::YARD, Length::CENTIMETER, 91.44,],
+            [1, Length::YARD, Length::METER, 0.9144,],
+            [1, Length::YARD, Length::KILOMETER, 0.0009144,],
+            [10, Length::YARD, Length::INCH, 360,],
+            [10, Length::YARD, Length::FEET, 30,],
+            [10, Length::YARD, Length::YARD, 10,],
+            [10, Length::YARD, Length::MILE, (10 / 1760),],
+            [10, Length::YARD, Length::MILLIMETER, 9144.0,],
+            [10, Length::YARD, Length::CENTIMETER, 914.4,],
+            [10, Length::YARD, Length::METER, 9.144,],
+            [10, Length::YARD, Length::KILOMETER, 0.009144,],
+            //from mile
+            [1, Length::MILE, Length::INCH, 63360,],
+            [1, Length::MILE, Length::FEET, 5280,],
+            [1, Length::MILE, Length::YARD, 1760,],
+            [1, Length::MILE, Length::MILE, 1,],
+            [1, Length::MILE, Length::MILLIMETER, 1609344,],
+            [1, Length::MILE, Length::CENTIMETER, 160934.4,],
+            [1, Length::MILE, Length::METER, 1609.344,],
+            [1, Length::MILE, Length::KILOMETER, 1.609344,],
+            [10, Length::MILE, Length::INCH, 633600,],
+            [10, Length::MILE, Length::FEET, 52800,],
+            [10, Length::MILE, Length::YARD, 17600,],
+            [10, Length::MILE, Length::MILE, 10,],
+            [10, Length::MILE, Length::MILLIMETER, 16093440,],
+            [10, Length::MILE, Length::CENTIMETER, 1609344,],
+            [10, Length::MILE, Length::METER, 16093.44,],
+            [10, Length::MILE, Length::KILOMETER, 16.09344,],
+            //from millimeter
+            [1, Length::MILLIMETER, Length::INCH, (1 / 25.4),],
+            [1, Length::MILLIMETER, Length::FEET, (1 / 304.8),],
+            [1, Length::MILLIMETER, Length::YARD, (1 / 914.4),],
+            [1, Length::MILLIMETER, Length::MILE, (1 / 1609344),],
+            [1, Length::MILLIMETER, Length::MILLIMETER, 1,],
+            [1, Length::MILLIMETER, Length::CENTIMETER, (1 / 10),],
+            [1, Length::MILLIMETER, Length::METER, (1 / 1000),],
+            [1, Length::MILLIMETER, Length::KILOMETER, (1 / 1e+6),],
+            [10, Length::MILLIMETER, Length::INCH, (10 / 25.4),],
+            [10, Length::MILLIMETER, Length::FEET, (10 / 304.8),],
+            [10, Length::MILLIMETER, Length::YARD, (10 / 914.4),],
+            [914.4, Length::MILLIMETER, Length::YARD, 1,],
+            [9144, Length::MILLIMETER, Length::YARD, 10,],
+            [91440, Length::MILLIMETER, Length::YARD, 100,],
+            [1609344, Length::MILLIMETER, Length::MILE, 1,],
+            [16093440, Length::MILLIMETER, Length::MILE, 10,],
+            [10, Length::MILLIMETER, Length::MILLIMETER, 10,],
+            [10, Length::MILLIMETER, Length::CENTIMETER, 1,],
+            [1000, Length::MILLIMETER, Length::METER, 1,],
+            [1000000, Length::MILLIMETER, Length::KILOMETER, 1,],
+            [10000000, Length::MILLIMETER, Length::KILOMETER, 10,],
+            [150545432, Length::MILLIMETER, Length::KILOMETER, 150.545432,],
+            //From centimeter
+            [1, Length::CENTIMETER, Length::INCH, (1 / 2.54),],
+            [1, Length::CENTIMETER, Length::FEET, (1 / 30.48),],
+            [1, Length::CENTIMETER, Length::YARD, (1 / 91.44),],
+            [1, Length::CENTIMETER, Length::MILE, (1 / 160934.4),],
+            [1, Length::CENTIMETER, Length::MILLIMETER, 10,],
+            [1, Length::CENTIMETER, Length::CENTIMETER, 1,],
+            [1, Length::CENTIMETER, Length::METER, (1 / 100),],
+            [1, Length::CENTIMETER, Length::KILOMETER, (1 / 100000.0),],
+            [10, Length::CENTIMETER, Length::INCH, (10 / 2.54),],
+            [10, Length::CENTIMETER, Length::FEET, (10 / 30.48),],
+            [10, Length::CENTIMETER, Length::YARD, (10 / 91.44),],
+            [91.44, Length::CENTIMETER, Length::YARD, 1,],
+            [914.4, Length::CENTIMETER, Length::YARD, 10,],
+            [9144, Length::CENTIMETER, Length::YARD, 100,],
+            [160934.4, Length::CENTIMETER, Length::MILE, 1,],
+            [1609344, Length::CENTIMETER, Length::MILE, 10,],
+            [10, Length::CENTIMETER, Length::MILLIMETER, 100,],
+            [10, Length::CENTIMETER, Length::CENTIMETER, 10,],
+            [100, Length::CENTIMETER, Length::METER, 1,],
+            [100000, Length::CENTIMETER, Length::KILOMETER, 1,],
+            [1000000, Length::CENTIMETER, Length::KILOMETER, 10,],
+            [15054543.2, Length::CENTIMETER, Length::KILOMETER, 150.545432,],
+
+            //From meter
+            [1, Length::METER, Length::INCH, (1 / 0.0254),],
+            [1, Length::METER, Length::FEET, 3.280839895,],
+            [1, Length::METER, Length::YARD, (1 / .9144),],
+            [1, Length::METER, Length::MILE, (1 / 1609.344),],
+            [1, Length::METER, Length::MILLIMETER, 1000,],
+            [1, Length::METER, Length::CENTIMETER, 100,],
+            [1, Length::METER, Length::METER, 1,],
+            [1, Length::METER, Length::KILOMETER, 0.001,],
+            [10, Length::METER, Length::INCH, (10 / 0.0254),],
+            [10, Length::METER, Length::FEET, (10 * 3.280839895),],
+            [10, Length::METER, Length::YARD, (10 / .9144),],
+            [0.9144, Length::METER, Length::YARD, 1,],
+            [9.144, Length::METER, Length::YARD, 10,],
+            [91.44, Length::METER, Length::YARD, 100,],
+            [1609.344, Length::METER, Length::MILE, 1,],
+            [16093.44, Length::METER, Length::MILE, 10,],
+            [10, Length::METER, Length::MILLIMETER, 10000,],
+            [10, Length::METER, Length::CENTIMETER, 1000,],
+            [10, Length::METER, Length::METER, 10,],
+            [1000, Length::METER, Length::KILOMETER, 1,],
+            [10000, Length::METER, Length::KILOMETER, 10,],
+            [150545.432, Length::METER, Length::KILOMETER, 150.545432,],
+
+            //From km
+            [1, Length::KILOMETER, Length::INCH, 39370.1,],
+            [1, Length::KILOMETER, Length::FEET, 3280.84,],
+            [1, Length::KILOMETER, Length::YARD, 1 / 0.0009144,],
+            [1, Length::KILOMETER, Length::MILE, 1 / 1.609344,],
+            [1, Length::KILOMETER, Length::MILLIMETER, 1000000,],
+            [1, Length::KILOMETER, Length::CENTIMETER, 100000,],
+            [1, Length::KILOMETER, Length::METER, 1000,],
+            [1, Length::KILOMETER, Length::KILOMETER, 1,],
+            [10, Length::KILOMETER, Length::INCH, 393701,],
+            [10, Length::KILOMETER, Length::FEET, 32808.4,],
+            [10, Length::KILOMETER, Length::YARD, 10 / 0.0009144,],
+            [10, Length::KILOMETER, Length::MILE, 10 / 1.609344,],
+            [10, Length::KILOMETER, Length::MILLIMETER, 10000000,],
+            [10, Length::KILOMETER, Length::CENTIMETER, 1000000,],
+            [10, Length::KILOMETER, Length::METER, 10000,],
+            [10, Length::KILOMETER, Length::KILOMETER, 10,],
+        ];
+    }
+}
